d50fa34fe040b872255f65d41777740baf48de3e
Jaysonzhao <jaysonzhao@vip.qq.com>
2019-11-08 11:41:38 +0800

    Update envvars.js

--- dance-ui/envvars.js
+++ dance-ui/envvars.js
@@ -1,5 +1,5 @@
-var URL_TRACKING_DEFAULT = "http://dancingrobot-robot-ml.apps.cluster-anz-7e19.anz-7e19.open.redhat.com"
-var URL_DANCEAPI_DEFAULT = "http://dance-translate-robot-dance.apps.cluster-anz-7e19.anz-7e19.open.redhat.com"
+var URL_TRACKING_DEFAULT = "http://mlflow-tracking-operator-robotdance.apps.cluster-6ec8.6ec8.sandbox936.opentlc.com/"
+var URL_DANCEAPI_DEFAULT = "http://dance-translate-robotdance.apps.cluster-6ec8.6ec8.sandbox936.opentlc.com/"
 //var URL_TRACKING = {{URL_TRACKING}};
 //var URL_DANCEAPI = {{URL_DANCEAPI}};
 
