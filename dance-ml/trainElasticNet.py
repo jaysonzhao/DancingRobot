5798c78fb2dd3d56a3004ebccf2b1ca470914b89
jaysonzhao <jaysonzhao@vip.qq.com>
2019-11-10 20:28:10 +0800

    lasso

--- dance-ml/trainElasticNet.py
+++ dance-ml/trainElasticNet.py
@@ -10,7 +10,7 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import Lasso
+from sklearn.linear_model import ElasticNet
 from io import StringIO
 
 import mlflow
@@ -129,25 +129,26 @@ def train_data(data, experimentName):
     train_y = train[["quality"]]
     test_y = test[["quality"]]
 
-    # alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 1.0
-    # l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 1.0
+    alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 1.0
+    l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 1.0
 
     with mlflow.start_run():
         global lr 
         global loadedModelName
-        lr = Lasso(alpha=alpha, tol=l1_ratio, random_state=38)
+        lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
         lr.fit(train_x, train_y)
 
         predicted_qualities = lr.predict(test_x)
 
         (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
 
-        print("Bayesian model :",  lr)
+        print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
         print("  RMSE: %s" % rmse)
         print("  MAE: %s" % mae)
         print("  R2: %s" % r2)
 
-        
+        mlflow.log_param("alpha", alpha)
+        mlflow.log_param("l1_ratio", l1_ratio)
         mlflow.log_metric("rmse", rmse)
         mlflow.log_metric("r2", r2)
         mlflow.log_metric("mae", mae)
