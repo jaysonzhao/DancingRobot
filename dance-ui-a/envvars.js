362dd18b759b89c4407053a09c6da76cf5752666
jaysonzhao <jaysonzhao@vip.qq.com>
2019-11-11 19:04:25 +0800

    add ui-a

---
+++ dance-ui-a/envvars.js
@@ -0,0 +1,6 @@
+var URL_TRACKING_DEFAULT = "http://dancingrobot-robotdance.apps.cluster-6ec8.6ec8.sandbox936.opentlc.com"
+var URL_DANCEAPI_DEFAULT = "http://dance-translate-robotdance.apps.cluster-6ec8.6ec8.sandbox936.opentlc.com"
+//var URL_TRACKING = {{URL_TRACKING}};
+//var URL_DANCEAPI = {{URL_DANCEAPI}};
+
+
